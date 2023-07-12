951c02c3fd07969f878c95a78579b77c28cb7d9a
jungheejung <heejung.jung@colorado.edu>
2023-07-12 15:57:46 -0400

    DEV: framewise displacement for pain runs

---
+++ fdmean_painruns.py
@@ -0,0 +1,7 @@
+# load .tsv as pandas
+# grab column name: framewise_displacement
+# average value of fd. np.mean()
+# use .at and save in new dataframe
+# %%
+# /dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep/results/fmriprep/sub-0071/ses-01/func
+
