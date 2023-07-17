0b605caa29b82b832f1d29274f226de988884948
Owen Collins <owencollins@Owens-MacBook-Pro.local>
2023-07-17 13:55:17 -0400

    DEV: code to merge tsv files

---
+++ mergedfdmean.py
@@ -0,0 +1,16 @@
+# %%
+import pandas as pd
+import glob
+#%%
+file_paths = sorted(glob.glob('/Users/owencollins/Desktop/summer_RA/data/fdmean/fdmean_sub*.tsv'))
+data_frames = []
+#%%
+for file_path in file_paths:
+    df = pd.read_csv(file_path, sep='\t')
+    data_frames.append(df)
+#%%
+merged_data = pd.concat(data_frames, ignore_index=True)
+merged_data.to_csv('/Users/owencollins/Desktop/mergedfdmean.tsv', sep='\t', index=False)
+
+
+# %%
